c3a7f3e6b3a825691f031f7195e1faf418f48bf6
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-16 00:45:57 +0100

    Added Home page into components

---
+++ components/NavBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <header
+    class="sticky top-0 z-05 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
+  >
+    <a href="" class="text-3xl fontmono">cartrader</a>
+  </header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
-    <NuxtPage />
+    <NavBar />
+    <CarHero />
 </div>
 
 </template>
