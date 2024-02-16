4d847d0ba19cec6c39df315bf21b37736e48c33e
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-16 09:58:23 +0100

    Loaded car data dynamically

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,21 +1,29 @@
+<script setup>
+  const props = defineProps({car:Object})
+
+
+</script>
+
+
 <template>
   <div
     class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+    @click="navigateTo(`/car/${car.name}-${car.id}`)"
   >
     <div class="flex h-full">
       <img
-        src="https://images.unsplash.com/photo-1599912027806-cfec9f5944b6?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        :src="car.url"
         alt=""
         class="w-[300px] h-full"
       />
       <div class="flex p-4 flex-col">
         <div>
-          <h1 class="text-2xl text-blue-700">Audi Q8</h1>
+          <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
           <p class="text-gray-700">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            {{ car.description }}
           </p>
         </div>
-        <h1 class="mt-auto text-xl">$81.799</h1>
+        <h1 class="mt-auto text-xl">${{ car.price }}</h1>
       </div>
     </div>
   </div>
