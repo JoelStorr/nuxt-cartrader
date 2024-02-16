43715e7a5bae45fc1cffff88f4cfac55c958a706
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-16 12:10:27 +0100

    Added useState to favorite cards

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,16 +1,29 @@
 <script setup>
+
+  import heartFilled from '@/assets/heartFilled.png';
+  import heartOutline from '@/assets/heartOutline.png';
+
   const props = defineProps({car:Object})
 
+ const favored = useState(`favored-${props.car.id}`, ()=>{
+  return false;
+ });
 
 </script>
 
 
 <template>
   <div
-    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
-    @click="navigateTo(`/car/${car.name}-${car.id}`)"
+    class="relative shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+   
   >
-    <div class="flex h-full">
+    <img 
+      class="absolute w-7 right-5 top-2 z-20" 
+      :src="favored ? heartFilled : heartOutline" 
+      alt=""
+      @click="favored = !favored"
+    />
+    <div class="flex h-full"  @click="navigateTo(`/car/${car.name}-${car.id}`)">
       <NuxtImg
         :src="car.url"
         alt=""
