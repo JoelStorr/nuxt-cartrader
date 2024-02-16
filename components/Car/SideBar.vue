c57266f3ac32f739099db28e32e48ebe80bab90d
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-16 13:51:34 +0100

    Added Price range

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -7,7 +7,27 @@ const modal = ref({
 });
 
 const city = ref("");
+const priceRange = ref({min:"", max:""})
 const route = useRoute();
+const router = useRouter();
+
+
+const priceRangeText = computed(()=>{
+  const minPrice = route.query.minPrice;
+  const maxPrice = route.query.maxPrice;
+
+
+  if(!minPrice && !maxPrice) return "Any"
+  else if(!minPrice && maxPrice){
+    return `< $${maxPrice}`
+  } else if(minPrice && !maxPrice){
+    return `> $${minPrice}`
+  } else {
+      return `$${minPrice}-$${maxPrice}`
+  }
+
+})
+
 
 function updateModal(key){
     modal.value[key] = !modal.value[key]
@@ -34,6 +54,20 @@ const onChnageMake = (make)=>{
 }
 
 
+const onChangePrice = ()=>{
+  updateModal('price')
+  if(priceRange.value.max && priceRange.value.min){
+    if(priceRange.value.min > priceRange.value.max) return;
+  }
+
+  router.push({
+    query:{
+      minPrice: priceRange.value.min,
+      maxPrice: priceRange.value.max
+    }
+  })
+}
+
 </script>
 
 <template>
@@ -59,9 +93,17 @@ const onChnageMake = (make)=>{
       </div>
     </div>
     <!-- Maks Ends -->
+    <!-- Price Ends -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Price</h3>
-      <h3 class="text-blue-400 capitalize">Any</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('price')"> {{ priceRangeText }} </h3>
+      <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white" v-if="modal.price">
+        <input class="border p-1 rounded" type="number" placeholder="Min" v-model="priceRange.min"/>
+       <input class="border p-1 rounded" type="number" placeholder="Max" v-model="priceRange.max"/>
+       <button class="bg-blue-400 w-full mt-2 rounded text-white p-1" @click="onChangePrice">Apply</button>
+        </div>
     </div>
+    <!-- Price Ends -->
   </div>
 </template>
+ 
